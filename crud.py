ee87ba76b207853a74d668c3540db44e352b5c9d
Sabira <Sabira_55@mail.ru>
2024-08-24 16:19:03 +0500

    реализация crud операций

---
+++ crud.py
@@ -0,0 +1,41 @@
+from sqlalchemy.orm import Session
+from . import models, schemas
+
+def create_user(db: Session, user: schemas.UserCreate):
+    db_user = models.User(**user.dict())
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+def get_user(db: Session, user_id: int):
+    return db.query(models.User).filter(models.User.id == user_id).first()
+
+def get_users(db: Session, skip: int = 0, limit: int = 10):
+    return db.query(models.User).offset(skip).limit(limit).all()
+
+def create_product(db: Session, product: schemas.ProductCreate):
+    db_product = models.Product(**product.dict())
+    db.add(db_product)
+    db.commit()
+    db.refresh(db_product)
+    return db_product
+
+def get_product(db: Session, product_id: int):
+    return db.query(models.Product).filter(models.Product.id == product_id).first()
+
+def get_products(db: Session, skip: int = 0, limit: int = 10):
+    return db.query(models.Product).offset(skip).limit(limit).all()
+
+def create_order(db: Session, order: schemas.OrderCreate):
+    db_order = models.Order(**order.dict())
+    db.add(db_order)
+    db.commit()
+    db.refresh(db_order)
+    return db_order
+
+def get_order(db: Session, order_id: int):
+    return db.query(models.Order).filter(models.Order.id == order_id).first()
+
+def get_orders(db: Session, skip: int = 0, limit: int = 10):
+    return db.query(models.Order).offset(skip).limit(limit).all()
